0f8230f1d0cb5cf03345cc71857b51206c6baeb2
Carlos <github.com/CarlosMartinezCM>
2023-09-26 07:02:27 -0700

    figured out how the voting works with views and how to handle the html layout

--- pickSite/pickSite/settings.py
+++ pickSite/pickSite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'bootstrap5',
+    #"home.apps.HomeConfig",
 ]
 
 MIDDLEWARE = [
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'US/Pacific'
 
 USE_I18N = True
 
