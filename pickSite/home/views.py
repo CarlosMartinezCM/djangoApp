1cf6d93fc303a15a2bfab4ee1c163db63efb642f
Carlos <github.com/CarlosMartinezCM>
2023-10-03 18:04:27 -0700

    Added some tests to test views and a home button in votes pages.

--- pickSite/home/views.py
+++ pickSite/home/views.py
@@ -1,7 +1,11 @@
+from typing import Any
+from django.db import models
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic 
+from django.utils import timezone
+
 
 from .models import Choice, Question
 
@@ -15,11 +19,16 @@ class IndexView(generic.ListView):
     
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:1]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
         
 class DetailView(generic.DetailView):
     model = Question
     template_name = "picks/detail.html"
+    """
+    Excludes any questions that aren't published yet. 
+    """
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
